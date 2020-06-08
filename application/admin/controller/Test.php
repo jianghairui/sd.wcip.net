3d4b89bb1cc322111f542484d378b9e810146e15
Jiang <1873645345@qq.com>
2020-06-08 17:10:55 +0800

    角色会员充值

--- application/admin/controller/Test.php
+++ application/admin/controller/Test.php
@@ -24,46 +24,46 @@ class Test extends Base {
 //    }
 
 
-//    private function index() {
-//        $uid = input('param.uid',0);
-//        if(!in_array($uid,[1,2,3])) {
-//            die('非法操作');
-//        }
-//        $whereRole = [
-//            ['uid','=',$uid]
-//        ];
-//        $whereUser = [
-//            ['id','=',$uid]
-//        ];
-//        try {
-//            $role_exist = Db::table('mp_user_role')->where($whereRole)->find();
-//            if(!$role_exist) {
-//                die('角色不存在');
-//            }
-//            @$this->rs_delete($role_exist['id_front']);
-//            @$this->rs_delete($role_exist['id_back']);
-//            @$this->rs_delete($role_exist['license']);
-//            @$this->rs_delete($role_exist['cover']);
-//            Db::table('mp_user_role')->where($whereRole)->delete();
-//            Db::table('mp_user')->where($whereUser)->update(
-//                [
-//                    'role' => 0,
-//                    'org' => ''
-//                    ]
-//            );
-//        } catch (\Exception $e) {
-//            return ajax($e->getMessage(), -1);
-//        }
-//
-//        echo 'id_front deleted<br>';
-//        echo 'id_back deleted<br>';
-//        echo 'license deleted<br>';
-//        echo 'cover deleted<br>';
-//        echo 'SUCCESS<br>';
-//
-//
-//
-//    }
+    private function index() {
+        $uid = input('param.uid',0);
+        if(!in_array($uid,[30])) {
+            die('非法操作');
+        }
+        $whereRole = [
+            ['uid','=',$uid]
+        ];
+        $whereUser = [
+            ['id','=',$uid]
+        ];
+        try {
+            $role_exist = Db::table('mp_user_role')->where($whereRole)->find();
+            if(!$role_exist) {
+                die('角色不存在');
+            }
+            @$this->rs_delete($role_exist['id_front']);
+            @$this->rs_delete($role_exist['id_back']);
+            @$this->rs_delete($role_exist['license']);
+            @$this->rs_delete($role_exist['cover']);
+            Db::table('mp_user_role')->where($whereRole)->delete();
+            Db::table('mp_user')->where($whereUser)->update(
+                [
+                    'role' => 0,
+                    'org' => ''
+                    ]
+            );
+        } catch (\Exception $e) {
+            return ajax($e->getMessage(), -1);
+        }
+
+        echo 'id_front deleted<br>';
+        echo 'id_back deleted<br>';
+        echo 'license deleted<br>';
+        echo 'cover deleted<br>';
+        echo 'SUCCESS<br>';
+
+
+
+    }
 
 
 
